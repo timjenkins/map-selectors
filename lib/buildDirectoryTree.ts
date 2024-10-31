e47baa1d753a7f198325885365fb590ea764e13c
Tim Jenkins <tim@timjenkins.me>
2024-10-31 17:05:40 -0400

    cleanup

--- lib/buildDirectoryTree.ts
+++ lib/buildDirectoryTree.ts
@@ -5,40 +5,6 @@ import type { Directory } from "../types.ts";
 import type { Tree, File } from "../types.ts";
 import { findFunctionContainingVariable } from "./regex/findEnclosingFunction.ts";
 
-function makeFileEntry(path: string): File | null {
-  const file: File = {
-    type: "file",
-    path,
-    name: path.split("/").pop()!,
-    selectors: getSelectorsFromFile(path),
-  };
-
-  return file.selectors.length > 0 ? file : null;
-}
-
-function getAbsoluteImportPath(
-  file: string,
-  selector: string,
-  filePath: string
-) {
-  const rawImportPath = findImportPath(file, selector);
-  if (!rawImportPath) return null;
-  return resolveRelativePath(rawImportPath, filePath);
-}
-
-function getSelectorsFromFile(path: string) {
-  const file = Deno.readTextFileSync(path);
-  const selectors = findSelectors(file) || [];
-
-  return [...selectors]
-    .map((selector) => {
-      const importPath = getAbsoluteImportPath(file, selector, path);
-      const calledWithin = findFunctionContainingVariable(file, selector) || "";
-      return importPath ? { selector, importPath, calledWithin } : null;
-    })
-    .filter((value) => !!value);
-}
-
 export function buildDirectoryTree(path: string) {
   const tree: Tree = {};
   const directory = Deno.readDirSync(path);
@@ -65,4 +31,38 @@ export function buildDirectoryTree(path: string) {
     }
   }
   return tree;
+}
+
+function makeFileEntry(path: string): File | null {
+  const file: File = {
+    type: "file",
+    path,
+    name: path.split("/").pop()!,
+    selectors: getSelectorsFromFile(path),
+  };
+
+  return file.selectors.length > 0 ? file : null;
+}
+
+function getSelectorsFromFile(path: string) {
+  const file = Deno.readTextFileSync(path);
+  const selectors = findSelectors(file) || [];
+
+  return [...selectors]
+    .map((selector) => {
+      const importPath = getAbsoluteImportPath(file, selector, path);
+      const calledWithin = findFunctionContainingVariable(file, selector) || "";
+      return importPath ? { selector, importPath, calledWithin } : null;
+    })
+    .filter((value) => !!value);
+}
+
+function getAbsoluteImportPath(
+  file: string,
+  selector: string,
+  filePath: string
+) {
+  const rawImportPath = findImportPath(file, selector);
+  if (!rawImportPath) return null;
+  return resolveRelativePath(rawImportPath, filePath);
 }
